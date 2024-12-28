5398fb53ea4312520adc3ee1d279051d0207b9ec
hqvjet <viethq.1906@gmail.com>
2024-12-29 03:04:18 +0700

    add test

--- test.py
+++ test.py
@@ -2,23 +2,23 @@ from vnindex import Preprocessing, Modelling, Chart
 import numpy as np
 import pandas as pd
 
+preprocessing = Preprocessing('res/dataset.csv')
+data = preprocessing.data
+print(data)
+
 # Draw charts
-data = pd.read_csv('res/dataset.csv')
-data = data.dropna()
 chart = Chart(data)
 # chart.ScatterPlot('VOL', 'CHANGE')
 # chart.ScatterPlot('PRICE', 'OPEN')
 chart.PairPlot()
 
-# preprocessing = Preprocessing('res/dataset.csv')
-# data = preprocessing.data
 # data = data.values
 #
 # model = Modelling(input=data, num_predict_date=1, num_date=14)
 
 # XGBOOST MODELLING
-model.xgboost.train()
-model.xgboost.eval()
+# model.xgboost.train()
+# model.xgboost.eval()
 
 # LSTM MODELLING
 # model.lstm.train()
